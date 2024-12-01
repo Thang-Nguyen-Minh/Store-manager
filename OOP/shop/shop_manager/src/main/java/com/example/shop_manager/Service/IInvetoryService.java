d2519eb16ae774f3fd93e40c76fc0907ec21b983
VuMinhHien1234 <vum257792@gmail.com>
2024-12-02 00:40:13 +0700

    Delete OOP directory

--- OOP/shop/shop_manager/src/main/java/com/example/shop_manager/Service/IInvetoryService.java
+++
@@ -1,6 +0,0 @@
-package com.example.shop_manager.Service;
-
-public interface IInvetoryService {
-	void loadProductsFromDatabase();
-	void searchProducts();
-}
--- OOP/shop/shop_manager/src/main/java/com/example/shop_manager/Service/IReportService.java
+++
@@ -1,6 +0,0 @@
-package com.example.shop_manager.Service;
-
-public interface IReportService {
-	void printAllInvoices();
-	void searchInvoices();
-}
